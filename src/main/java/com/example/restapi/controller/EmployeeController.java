1a61b9f07648efd60fe710d8f95cf7c71cad246b
Loc Nguyen <Loc.Nguyen@sandata.com>
2020-05-10 22:47:10 +0700

    fix comments

--- src/main/java/com/example/restapi/controller/EmployeeController.java
+++ src/main/java/com/example/restapi/controller/EmployeeController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/employee")
+@RequestMapping("/employees")
 public class EmployeeController {
 
 	@Autowired

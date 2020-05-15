6fc94ce4fd1f3feedd94709a8294942016fd44c3
heroloc89 <phuocloc1989@gmail.com>
2020-05-15 19:33:07 +0700

    jwt - cont

---
+++ src/main/java/com/example/restapi/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,41 @@
+package com.example.restapi.jwt;
+
+import com.example.restapi.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    JwtTokenProvider tokenProvider;
+
+    @Autowired
+    UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String jwt = getJwtFromRequest(request);
+        if(StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+            Long userId = tokenProvider.getUserIdFromJWT(jwt);
+//            UserDetails userDetails = userService.loadUserByUsername(userId);
+        }
+    }
+
+    private String getJwtFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+}

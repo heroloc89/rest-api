e6e665d1baf4f354f1a02d2c2d191f6e3c37ab02
heroloc89 <phuocloc1989@gmail.com>
2020-05-20 14:28:24 +0700

    unit test

--- src/main/java/com/example/restapi/entities/User.java
+++ src/main/java/com/example/restapi/entities/User.java
@@ -14,8 +14,8 @@ import javax.persistence.Table;
 public class User{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
     private String username;
     private String password;
     private String role;
